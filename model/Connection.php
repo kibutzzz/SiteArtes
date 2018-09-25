c72852155dbfd4452120227941d7f7e5914f4195
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-09-25 10:56:11 -0300

    adicionadas as funções executeQuery, closeConnection e selectAllProjects

--- model/Connection.php
+++ model/Connection.php
@@ -1,26 +1,47 @@
 <?php
 
-Class Connection extends mysqli{
+Class Connection {
 
     private $connection;
     private $sql;
 
     public function __construct() {
-        define("DB_NAME", 'a-parede');
+        define("DB_NAME", 'parede-database');
         define("DB_PASSWORD", '');
         define("DB_HOST", 'localhost');
         define("DB_USER", 'root');
+        define("TABLE_PROJETOS", 'projetos');
         
-
+        
         $this->connect();
     }
 
     private function connect() {
         $this->connection = new mysqli(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+        
+          
         if ($this->connection->connect_errno) {
             echo "Erro de conexão: " . $this->connection->connect_error;
         }
     }
        
+    public function selectAllProjects() {
+        $this->sql = "SELECT * FROM " . TABLE_PROJETOS . " ;";
+        
+        return $this->executeQuery();
+    }
+    
+    private function executeQuery() {
 
+        $res = $this->connection->query($this->sql);
+        if ($this->connection->errno) {
+            return "Erro de query: " . $this->connection->error;
+        }
+        
+        return $res;
+    }
+    
+     public function closeConnection() {
+        $this->connection->close();
+    }
 }
