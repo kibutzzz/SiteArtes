3ba7e5743236abe7f52bb92575a30b5ecc9d3a17
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-09-25 10:55:03 -0300

    adicionadas funçoes getProjects e closeConnection

--- controllers/ControllerPaginaInicial.php
+++ controllers/ControllerPaginaInicial.php
@@ -2,12 +2,29 @@
 require "Controller.php";
 
 class ControllerPaginaInicial extends Controller {
-    
+    private $connection;
     function __construct() {
         parent::__construct();
     
                 
+        require_once '../model/Connection.php';
+        $this->connection = new Connection();        
+    }
+    
+    
+    function getProjects(){
+        $projects = [];
+        $res = $this->connection->selectAllProjects();
         
+        while ($proj = $res->fetch_assoc()) {
+            array_push($projects, $proj);
+        }
+        
+        return $projects;
+    }
+    
+    function closeConnection() {
+        $this->connection->closeConnection();
     }
     
 }
