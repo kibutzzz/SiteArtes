dd2b9cade8daf8703156d058af4997ce574265e1
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-09-25 08:35:19 -0300

    mudado o titulo padrao da pagina e o icone padrao

--- controllers/Controller.php
+++ controllers/Controller.php
@@ -15,8 +15,8 @@ class Controller {
         $this-> styles= [
             '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">',
         ];
-        $this->pageTitle = "Nova Página";
-        $this->iconPath = "../img/mvc.png";
+        $this->pageTitle = "A parede";
+        $this->iconPath = "../img/logo.png";
     }
     function setPageTitle($pageTitle) {
         $this->pageTitle = $pageTitle;
