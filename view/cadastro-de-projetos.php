beadf338504b11a9a340a57a567dc0e871f96de9
Leonardo Ramos <leonardo.meinerzramos28@gmail.com>
2018-09-28 10:39:20 -0300

    Adicionado parte do formulario de cadastro de projeto

--- view/cadastro-de-projetos.php
+++ view/cadastro-de-projetos.php
@@ -14,16 +14,53 @@ $controller = new ControllerCadastroDeProjetos();
         ?>
     </head>
     <body>
-        
-            <?php require "./modulos/menu-navegacao.php"; ?>
-        <div class="container">
-            
+
+        <?php require "./modulos/menu-navegacao.php"; ?>
+        <div class="container mt-3">
+            <form action="../controllers/form-handlers/cadastro-de-projeto.php" method="POST">
+                <h2>Cadastro do projeto</h2>
+                <div class="form-group row">
+                    <label for="inputNome" class="col-sm-2 col-form-label">Nome</label>
+                    <div class="col-sm-10">
+                        <input type="text" class="form-control"  placeholder="Nome do projeto">
+                    </div>
+                </div>
+                <div class="form-group row">
+                    <label class="col-sm-2 col-form-label" for="selectTurma">Turma</label>
+                    <div class="col-sm-10">
+                        <select class="form-control">
+                            <option>Informática 1° ano</option>
+                            <option>Informática 2° ano</option>
+                            <option>Informática 3° ano</option>
+                            <option>Informática 4° ano</option>
+                            <option>Eletromecanica 1°</option>
+                            <option>Eletromecanica 2°</option>
+                            <option>Eletromecanica 3°</option>
+                            <option>Eletromecanica 4°</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <label for="resumo">Resumo:</label>
+                    <textarea class="form-control" rows="10" ></textarea>
+                </div>
+
+
+
+                <div class="form-group row">
+                    <div class="col-sm-10">
+                        <button type="submit" class="btn btn-primary">Sign in</button>
+                    </div>
+                </div>
+                
+                <!--TODO terminar formulario com o cadastro de participantes do projeto-->
+            </form>
         </div>
     </body>
     <?php
     foreach ($controller->getScripts() as $script) {
         echo $script;
     }
-    
     ?>
 </html>
