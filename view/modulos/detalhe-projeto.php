fca5357f11ea59b1c89b5fe40aa060defb0e9961
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-10-13 18:04:31 -0300

    adicionado o modulo que mostra o card com aos detalhes do projeto

---
+++ view/modulos/detalhe-projeto.php
@@ -0,0 +1,10 @@
+<div class="card mt-4" >
+            <!--<img class="card-img-top" src="#" alt="foto do projeto">-->
+    <div class="card-body">
+        <h5 class="card-title"><?= $project['project_details']['nome'] ?></h5>
+        <p class="card-text"><?= $project['project_details']['resumo'] ?></p>
+        <p class="card-text"><?= $project['project_details']['data_cadastro'] ?></p>
+        
+        
+    </div>
+</div>
