bed1f235a20c23a4040fa3bbabd5983fb0cb2b75
Leonardo Meinerz Ramos <leonardo.meinerzramos28@gmail.com>
2018-10-20 19:39:32 -0300

    adicionado condições para mostrar opções do menu de acordo com as variaveis de sessão

--- view/modulos/menu-navegacao.php
+++ view/modulos/menu-navegacao.php
@@ -1,18 +1,37 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="./pagina-inicial.php"A> parede</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item">
-        <a class="nav-link" href="./cadastro-de-projetos.php">Cadastro</a>
-      </li>      
-    </ul>
-      <ul class="navbar-nav ">
-          <li class="nav-item">
-              <a class="nav-link" href="./login.php">Login</a>
-          </li>
-      </ul>
-  </div>
+    <a class="navbar-brand" href="./pagina-inicial.php"A> parede</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="./cadastro-de-projetos.php">Cadastro</a>
+            </li>      
+        </ul>
+        <ul class="navbar-nav ">
+            <?php
+            if ($controller->session_handler->getValue('logado')) {
+                ?> 
+                <li class="nav-item">
+                    <a class="nav-link" href="./administracao.php">Área administrativa</a>
+                </li>
+                <?php
+            }
+            ?>
+            <li class="nav-item">
+                <?php
+                if ($controller->session_handler->getValue('logado')) {
+                    ?>
+                    <a class="nav-link" href="./logout.php">sair</a>
+                    <?php
+                } else {
+                    ?>
+                    <a class="nav-link" href="./login.php">Login</a>
+                    <?php
+                }
+                ?>
+            </li>
+        </ul>
+    </div>
 </nav>
