270cfd4993d3c5de9b1066d18c4faf4d1b6c5041
Leonardo Ramos <leonardo.meinerzramos28@gmail.com>
2018-10-24 10:37:26 -0300

    adicionado redirecionamento para pagina inicial caso o usuario não esteja logado

--- controllers/ControllerAdministracao.php
+++ controllers/ControllerAdministracao.php
@@ -5,7 +5,10 @@ class ControllerAdministracao extends Controller {
     
     function __construct() {
         parent::__construct();
-        
+        $this->session_handler->setRedirectUrl(" ./pagina-inicial.php");
+        if(!$this->session_handler->getValue('logado')){
+            header("location: " . $this->session_handler->getRedirectUrl());
+        }
     }
     
 }
