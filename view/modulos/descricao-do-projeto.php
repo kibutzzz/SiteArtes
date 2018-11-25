8ffac6c71ff9556878c2924539319b0adf704bda
Leonardo <leonardo.meinerzramos28@gmail.com>
2018-11-25 19:14:53 -0200

    adicionado modulo de descricao do projeto a parede

---
+++ view/modulos/descricao-do-projeto.php
@@ -0,0 +1,24 @@
+
+<h2 class="text-center mt-3 mb-3">Apresentação do Projeto</h2>
+
+
+<p class="text-justify"> A PAREDE: ideias que se aderem é a ocupação de uma parede livre e disponível do Câmpus Sapiranga para a realização de intervenções artísticas transitórias e efêmeras em desenho e pintura mediante inscrição de equipes de alunos interessados com a supervisão e acompanhamento do coordenador. Para isso o projeto terá um calendário próprio e anual com regras de ocupação, montagem/desmontagem, prazos e materiais adequados. 
+A Parede: ideias que se aderem quer estimular a expressão dos participantes como enriquecer a observação e a curiosidade dos passantes em franco diálogo com o entorno paisagístico e arquitetônico do campus e arredores. </p> 
+
+<p class="text-justify">
+Objetivo Geral do Projeto: Promover a ocupação e a ativação de uma parede do campus com intervenções em desenho e pintura ao fomentar o estímulo à criatividade e a prática de trabalhos em equipe, como de encontrar soluções gráficas e pictóricas a serem realizadas num curto espaço de tempo envolvendo produção, exposição e o seu desaparecimento.</p>
+
+
+<h5 class="text-center mb-4">As imagens abaixo demonstram o "antes e o depois" da parede escolhida para representar o projeto.</h5>
+
+<div class="row mb-3">
+	
+    <div class="col-sm-6">
+        <img src="..\raw\parede-antes.jpg" class="img-fluid">
+    </div>
+
+    <div class="col-sm-6">
+        <img src="../raw/parede-depois.jpg" class="img-fluid">
+    </div>
+
+</div>
