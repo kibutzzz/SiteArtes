dbc69623448a13fa1fed2907994bb752d11722c1
Leonardo <leonardo.meinerzramos28@gmail.com>
2018-11-25 19:14:33 -0200

    corrigida a pagina inicial

--- view/pagina-inicial.php
+++ view/pagina-inicial.php
@@ -3,31 +3,31 @@
 require "../controllers/ControllerPaginaInicial.php";
 $controller = new ControllerPaginaInicial();
 ?>
-<html lang="pt-br">
+<!doctype html>
+<html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title><?= $controller->getPageTitle() ?></title>
         <link rel="shortcut icon" type="image/png" href="<?= $controller->getIconPath() ?>"/>
-        <?php
-        foreach ($controller->getStyles() as $style) {
-            echo $style;
-        }
-        ?>
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     </head>
     <body>
-
-        <?php require "./modulos/menu-navegacao.php"; ?>
+    <?php 
+        require "modulos/menu-navegacao.php";
+    ?>
         <div class="container">
-            <?php require "./modulos/feed-projetos.php"; ?>
-        </div>
+            <?php
+                require "modulos/descricao-do-projeto.php";
+            ?>
+        </div>	
+
     </body>
     <?php
     foreach ($controller->getScripts() as $script) {
         echo $script;
     }
-    $controller->closeConnection();
-    ?>
     
-
+    ?>
 </html>
