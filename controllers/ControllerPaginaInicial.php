328d6f506dcdc0f3038d5282aeeb31818c5fac34
Leonardo <leonardo.meinerzramos28@gmail.com>
2018-11-25 19:13:32 -0200

    removidas as funções desnecessarias e passadas para a o controller de feed de projetos

--- controllers/ControllerPaginaInicial.php
+++ controllers/ControllerPaginaInicial.php
@@ -3,37 +3,16 @@ require "Controller.php";
 
 class ControllerPaginaInicial extends Controller {
     
-    private $connection;
+    
 
     function __construct() {
         parent::__construct();
     
         $this->setPageTitle("Pagina inicial | A parede");  
-
                 
-        require_once '../model/Connection.php';
-        $this->addStyle("<link href='../style/pagina-inicial.css' rel='stylesheet'>");
-        $this->connection = new Connection();        
+      
+             
     }
     
-    
-    function getProjects(){
-        $projects = [];
-        $res = $this->connection->selectAllApprovedProjects();
-        
-        while ($proj = $res->fetch_assoc()) {
-            array_push($projects, $proj);
-        }
-        
-        return $projects;
-    }
-    
-    function closeConnection() {
-        $this->connection->closeConnection();
-    }
-
-    function hasProjects(){
-        return $this->connection->countAllAprovedProjects()->fetch_assoc()['count(*)'] > 0;
-    }
     
 }
