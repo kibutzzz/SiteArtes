0c44e31741c1dfc057349ad823dafdf336b7a091
Leonardo <leonardo.meinerzramos28@gmail.com>
2018-11-25 18:43:14 -0200

    adicionada pagina separada para o feed de projetos

---
+++ view/feed-projetos.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<?php
+require "../controllers/ControllerFeedProjetos.php";
+$controller = new ControllerFeedProjetos();
+?>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title><?= $controller->getPageTitle() ?></title>
+        <link rel="shortcut icon" type="image/png" href="<?= $controller->getIconPath() ?>"/>
+        <?php
+        foreach ($controller->getStyles() as $style) {
+            echo $style;
+        }
+        ?>
+    </head>
+    <body>
+
+        <?php require "./modulos/menu-navegacao.php"; ?>
+        <div class="container">
+            <?php require "./modulos/feed-projetos.php"; ?>
+        </div>
+    </body>
+    <?php
+    foreach ($controller->getScripts() as $script) {
+        echo $script;
+    }
+    $controller->closeConnection();
+    ?>
+    
+
+</html>
+
