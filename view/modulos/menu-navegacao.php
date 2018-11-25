274628809ad2040b84c854a25fb902f8c154cd2d
Leonardo <leonardo.meinerzramos28@gmail.com>
2018-11-25 18:44:37 -0200

    adicionado links para as regras e para o feed de projetos

--- view/modulos/menu-navegacao.php
+++ view/modulos/menu-navegacao.php
@@ -1,10 +1,13 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-dark  bg-dark">
     <a class="navbar-brand" href="./pagina-inicial.php"A> Parede</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
         <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="feed-projetos.php">Feed de Projetos</a>
+            </li>
             <li class="nav-item">
                 <a class="nav-link" href="./cadastro-de-projetos.php">Cadastro</a>
             </li>    
@@ -13,7 +16,11 @@
             </li> 
             <li class="nav-item">
                 <a class="nav-link" href="./jingle.php">Jingle</a>
+            </li>  
+            <li class="nav-item">
+                <a class="nav-link" href="https://docs.google.com/document/d/1tUc9BypTQmDSbhB7aUbSrPbkGrOSJ6GI3bosB_QF_Nc/edit?usp=sharing">Regras</a>
             </li>   
+            
         </ul>
         <ul class="navbar-nav ">
             <?php
